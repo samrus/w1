cf22ad651eb28346b268472d46a3537720383d8b
Alexander Skornyakov <skornyakov@outlook.com>
2016-04-24 16:28:21 +0400

    Task example

--- w1/Program.cs
+++ w1/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Windows.Forms;
 using System.Threading.Tasks;
+using System;
 
 namespace w1
 {
@@ -10,7 +8,16 @@ namespace w1
     {
         static void Main(string[] args)
         {
-            //
+            Task task = new Task(new Action(ShowDialog));
+            task.Start();
+           
+            Console.WriteLine("Dialog 1");
+            task.Wait();
+        }
+
+        private static void ShowDialog()
+        {
+            MessageBox.Show("Dialog");
         }
     }
 }
